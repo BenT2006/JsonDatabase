adb8b542f92a0ff085803dab207674eefd44903c
BenT2006 <anbinh.benjamin@gmail.com>
2020-10-15 19:41:13 -0700

    commit number 1d

---
+++ BenWeb/src/MyHello/Compute.java
@@ -0,0 +1,13 @@
+package MyHello;
+
+public class Compute {
+
+	public Compute() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String print() {
+		return "\nhello testing 123 --- =[];'";
+	}
+
+}
